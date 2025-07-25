1ea998bfb687e92ddca5f3f584359cd1822dd396
Juan Pablo Gonzalez Magaldi <juanpablogmagaldi@gmail.com>
2025-07-25 12:21:33 -0300

    Cambios
    cambios

---
+++ src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Appbar from '../components/appbar'
+
+
+export default function Home() {
+    
+    return (
+        <>
+           <Appbar/>
+                <h3 >
+                    PAGINA PRINCIPAL
+                </h3>
+        </>
+    )
+}
